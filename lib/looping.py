d6d393c24b2e8d36df0d5b0d045690bbee34065f
MartinWakaba <martin.wakaba@student.moringaschool.com>
2024-05-29 22:26:12 +0300

    done

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,31 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    countdown = 10
+    while countdown >= 1:
+        print(countdown)
+        countdown -= 1
+    print("Happy New Year!")
+
+happy_new_year()
+
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    squared_nums = [num1 ** 2 for num1 in int_list]
+    return squared_nums
+
+
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for num in range (1, 101):
+        if (num%3 == 0 and num%5 == 0):
+            print("FizzBuzz")
+        elif (num % 3 == 0):
+            print("Fizz")
+        elif (num % 5 == 0):
+            print("Buzz")
+        else:
+            print(num)
+fizzbuzz()
